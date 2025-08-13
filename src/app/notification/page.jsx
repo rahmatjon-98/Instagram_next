2e3f622bdc37821b0f5a8c8cc8b2503b12bb1c1e
norovskiy <muhammadnorovl83@gmail.com>
2025-08-13 13:37:25 -0700

    cimmit

--- src/app/notification/page.jsx
+++ src/app/notification/page.jsx
@@ -9,18 +9,8 @@ export default function UsersList() {
 	const [tokenReady, setTokenReady] = useState(false)
 
 	useEffect(() => {
-		localStorage.setItem(
-			'access_token',
-			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiJiYTIwOWZhMi1lZWNjLTQ0MGQtOWU2ZC1hMzcyYzY3Y2RlZmEiLCJuYW1lIjoiX25vcm92c2tpeV8iLCJlbWFpbCI6Im5vcm92c2tpeUBnbWFpbC5jb20iLCJzdWIiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJVc2VyIiwiZXhwIjoxNzU1MDc5NjQ0LCJpc3MiOiJpbnN0YWdyYW0tZ3JvdXAiLCJhdWQiOiJpbnN0YWdyYW0tYXBpIn0.W9VcDImjN7-9UxslWoRk1GH0lR1ePF4vr_pVcgjbIg0'
-		)
-		setTokenReady(true)
+		get()
 	}, [])
-
-	useEffect(() => {
-		if (tokenReady) {
-			get()
-		}
-	}, [tokenReady])
 
 	if (loading) return <p>Загрузка...</p>
 	if (error) return <p>Ошибка: {error}</p>
@@ -35,7 +25,7 @@ export default function UsersList() {
 							src={
 								user.avatar
 									? `http://37.27.29.18:8003/images/${user.avatar}`
-									: '/profile_default.png' 
+									: '/profile_default.png'
 							}
 							alt={user.fullName || 'User Avatar'}
 							width={100}
