6846387ea632cb932397431a2371c7aa7c8e216e
Yoqub573 <yokub573@gmail.com>
2025-08-16 18:36:57 +0500

    add

--- src/components/pages/home/stories.jsx
+++ src/components/pages/home/stories.jsx
@@ -1,80 +1,55 @@
 'use client'
 import { useHome } from '@/store/pages/home/store'
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState } from 'react'
 import Stories from 'react-insta-stories'
-const burakHeading = {
-	heading: 'Umar',
-	subheading: 'umar@gmail.com',
-	profileImage: 'https://i.imgur.com/lq0DC3a.jpg',
-}
-const initialStories = [
-	{
-		url: 'https://i.imgur.com/QpUEcfi.jpg',
-		type: 'image',
-		header: burakHeading,
-	},
-	{
-		url: 'https://i.imgur.com/in5Jr1h.jpg',
-		type: 'image',
-		header: burakHeading,
-	},
-	{
-		url: 'https://i.imgur.com/Zo5Kpnd.mp4',
-		type: 'video',
-		header: burakHeading,
-	},
-	{
-		url: 'https://i.imgur.com/LBRXhIq.jpg',
-		type: 'image',
-		header: burakHeading,
-	},
-	{
-		url: 'https://i.imgur.com/ARMxyC4.png',
-		type: 'image',
-		header: burakHeading,
-	},
-]
-const StoryComponent = ({idSoryUser}) => {
-	let {getUserStories,data} = useHome()
-	useEffect(()=>{})
-	const [stories, setStories] = React.useState(initialStories)
 
-	const onAllStoriesEndHandler = () => {
-		console.log('stories ended')
-	}
+const StoryComponent = ({ idStoryUser }) => {
+  const { getUserStories, data } = useHome()
+  const [stories, setStories] = useState([{ content: () => <div /> }])
 
-	const storyContent = {
-		width: 'auto',
-		maxWidth: '100%',
-		maxHeight: '100%',
-		margin: 'auto',
-	}
+  useEffect(() => {
+    getUserStories()
+  }, [])
 
-	return (
-		<React.Fragment>
-			<Stories
-				stories={stories}
-				defaultInterval={2000}
-				// width={'100%'}
-				// height={'100%'}
-				style={{
-					display: 'flex',
-					justifyContent: 'center',
-					cursor: 'pointer',
-					borderRadius: '20px',
-				}}
-				storyStyles={storyContent}
-				loop={false}
-				keyboardNavigation={true}
-				isPaused={() => {}}
-				currentIndex={() => {}}
-				onStoryStart={() => {}}
-				onStoryEnd={() => {}}
-				onAllStoriesEnd={onAllStoriesEndHandler}
-			/>
-		</React.Fragment>
-	)
+  useEffect(() => {
+    if (data && idStoryUser) {
+      const user = data.find(u => u.userId === idStoryUser)
+      if (user && user.stories?.length) {
+        const burakHeading = {
+          heading: user.username || 'User',
+          subheading: user.email || '',
+          profileImage: user.avatar || 'https://i.imgur.com/lq0DC3a.jpg',
+        }
+
+        const mappedStories = user.stories.map(el => ({
+          url: `http://37.27.29.18:8003/images${el.fileName}`,
+          type: el.fileName.endsWith('.mp4') ? 'video' : 'image',
+          header: burakHeading,
+        }))
+
+        setStories(mappedStories)
+      } else {
+        // если сторис нет → безопасный пустой story
+        setStories([{ content: () => <div style={{ background: '#000', height: '100%' }} /> }])
+      }
+    }
+  }, [data, idStoryUser])
+
+  const onAllStoriesEndHandler = () => {
+    console.log('stories ended')
+  }
+
+  return (
+    <div className="w-[380px] max-w-[380px] aspect-[9/16] flex justify-center">
+      <Stories
+        stories={stories}
+        defaultInterval={2000}
+        storyStyles={{ borderRadius: '20px' }}
+        loop={false}ё
+        onAllStoriesEnd={onAllStoriesEndHandler}
+      />
+    </div>
+  )
 }
 
 export default StoryComponent
