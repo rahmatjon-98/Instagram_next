6200073a88b2886524a31b6a798e10ff72f9f1d0
Muhammad <mmarupov27@gmail.com>
2025-08-18 14:48:07 +0500

    norov

--- src/app/setting/layout.jsx
+++ src/app/setting/layout.jsx
@@ -6,21 +6,31 @@ import { ALargeSmall, ArrowDownToLine, AtSign, Baby, BadgeCheck, Ban, Bell, Bell
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useTranslation } from 'react-i18next'
+import useDarkSide from '@/hook/useDarkSide'
 
 const LayoutSet = ({ children }) => {
     const pathname = usePathname();
     const { t } = useTranslation();
     const [open, setOpen] = useState(false)
+    const [theme, setTheme] = useDarkSide()
 
     return (
         <div className="flex flex-col md:flex-row lg:gap-[10px]">
-            <div className="w-full md:w-[320px] lg:w-[400px] h-auto md:h-screen p-[20px] overflow-y-auto md:scroll-smooth">
+
+            <div className={`
+                fixed top-0 left-0 h-screen z-50 transition-transform duration-300 
+                md:relative md:h-screen md:translate-x-0
+                ${open ? "translate-x-0" : "-translate-x-full"} 
+                md:w-[320px] lg:w-[400px] w-full p-[20px] overflow-y-auto scroll-smooth
+            `}>
+
+                <div className="flex justify-end md:hidden mb-4">
+                    <button onClick={() => setOpen(false)} className="p-2">
+                        <X size={24} />
+                    </button>
+                </div>
 
                 <p className='font-semibold text-[22px]'>{t('setting.settings')}</p>
-                <button onClick={() => setOpen(!open)} className="p-2">
-                    {open ? <X size={24} /> : <Menu size={24} />}
-                </button>
-
                 <div className='p-4 w-full md:w-[300px] mt-[20px] shadow-xl rounded-xl font-sans transition-transform duration-300 ease-in-out hover:scale-101'>
                     <Image
                         className="w-[100px] h-[110px] transition-transform duration-300 ease-in-out hover:scale-105"
@@ -56,15 +66,14 @@ const LayoutSet = ({ children }) => {
                 </div>
 
                 <p className='font-semibold text-[15px] text-gray-600 w-[300px] mt-[30px]'>{t('setting.insta')}</p>
-
                 <Link href={'/setting/pro'}>
-                    <div className={`flex gap-[10px] items-center p-4 rounded-md mt-[20px] transition-colors duration-300 ${pathname === "/setting/pro" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-4 rounded-md mt-[20px] transition-colors duration-300 ${pathname === "/setting/pro" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <CircleUserRound />
                         <p>{t('setting.profile')}</p>
                     </div>
                 </Link>
                 <Link href={'/setting/likes'}>
-                    <div className={`flex gap-[10px] items-center p-4 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/likes" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-4 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/likes" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Bell />
                         <p>{t('setting.notification')}</p>
                     </div>
@@ -72,25 +81,25 @@ const LayoutSet = ({ children }) => {
 
                 <p className='font-semibold text-[15px] text-gray-600 mt-[30px]'>{t('setting.content')}</p>
                 <Link href={'/setting/block-account'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/block-account" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/block-account" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Lock />
                         <p>Конфиденциальность <br /> аккаунта</p>
                     </div>
                 </Link>
                 <Link href={'/setting/blizki'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/blizki" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/blizki" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <CircleStar />
                         <p>Близкие друзья</p>
                     </div>
                 </Link>
                 <Link href={'/setting/block'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/block" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/block" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Ban />
                         <p>Заблокированные</p>
                     </div>
                 </Link>
                 <Link href={'/setting/story'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/story" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/story" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <CircleOff />
                         <p>Скрыть историю</p>
                     </div>
@@ -98,37 +107,37 @@ const LayoutSet = ({ children }) => {
 
                 <p className='font-semibold text-[15px] text-gray-600 mt-[30px]'>Взаимодействие с вами</p>
                 <Link href={'/setting/message'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/message" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/message" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <MessageCircleReply />
                         <p>Сообщения и ответы на <br /> истории</p>
                     </div>
                 </Link>
                 <Link href={'/setting/upr'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/upr" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/upr" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <AtSign />
                         <p>Метки и упоминания</p>
                     </div>
                 </Link>
                 <Link href={'/setting/comment'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/comment" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/comment" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <MessageCircleDashed />
                         <p>Комментарии</p>
                     </div>
                 </Link>
                 <Link href={'/setting/repost'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/repost" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/repost" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Repeat />
                         <p>Репосты и повторное <br /> использование</p>
                     </div>
                 </Link>
                 <Link href={'/setting/RestrictedAccount'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/RestrictedAccount" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/RestrictedAccount" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <UserX />
                         <p>Аккаунты с ограничениями</p>
                     </div>
                 </Link>
                 <Link href={'/setting/words'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/words" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/words" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <ALargeSmall />
                         <p>Скрытые слова</p>
                     </div>
@@ -136,25 +145,25 @@ const LayoutSet = ({ children }) => {
 
                 <p className='font-semibold text-[15px] text-gray-600 mt-[30px]'>Что вы видите</p>
                 <Link href={'/setting/accountes-block'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/accountes-block" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/accountes-block" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <BellOff />
                         <p>Скрытые аккаунты</p>
                     </div>
                 </Link>
                 <Link href={'/setting/setContent'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/setContent" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/setContent" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <SquarePlay />
                         <p>Настройки контента</p>
                     </div>
                 </Link>
                 <Link href={'/setting/heart'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/heart" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/heart" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <HeartOff />
                         <p>Число отметок "Нравится" <br /> и репостов</p>
                     </div>
                 </Link>
                 <Link href={'/setting/corol'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/corol" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/corol" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Crown />
                         <p>Платные подписки</p>
                     </div>
@@ -162,19 +171,19 @@ const LayoutSet = ({ children }) => {
 
                 <p className='font-semibold text-[15px] text-gray-600 mt-[30px]'>Ваше приложение и медиафайлы</p>
                 <Link href={'/setting/download'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/download" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/download" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <ArrowDownToLine />
                         <p>Архивирование и <br /> скачивание</p>
                     </div>
                 </Link>
                 <Link href={'/setting/baby'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/baby" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/baby" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Baby />
                         <p>Специальные возможности</p>
                     </div>
                 </Link>
                 <Link href={'/setting/language'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/language" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/language" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Languages />
                         <p>Язык</p>
                     </div>
@@ -188,7 +197,7 @@ const LayoutSet = ({ children }) => {
 
                 <p className='font-semibold text-[15px] text-gray-600 mt-[30px]'>Семьям</p>
                 <Link href={'/setting/family'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/family" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/family" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <Users />
                         <p>Семейный центр</p>
                     </div>
@@ -196,13 +205,13 @@ const LayoutSet = ({ children }) => {
 
                 <p className='font-semibold text-[15px] text-gray-600 mt-[30px]'>Для профессиональных аккаунтов</p>
                 <Link href={'/setting/accountType'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/accountType" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/accountType" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <SquareChartGantt />
                         <p>Тип аккаунта и <br /> инструменты</p>
                     </div>
                 </Link>
                 <Link href={'/setting/show'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/show" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/show" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <BadgeCheck />
                         <p>Покажите, что ваш <br /> профиль подтвержден</p>
                     </div>
@@ -210,27 +219,40 @@ const LayoutSet = ({ children }) => {
 
                 <p className='font-semibold text-[15px] text-gray-600 mt-[30px]'>Информация и поддержка</p>
                 <Link href={'/setting/help'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/help" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[15px] transition-colors duration-300 ${pathname === "/setting/help" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <LifeBuoy />
                         <p>Помощь</p>
                     </div>
                 </Link>
                 <Link href={'/setting/center'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/center" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/center" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <ShieldPlus />
                         <p>Центр <br /> конфиденциальности</p>
                     </div>
                 </Link>
                 <Link href={'/setting/statusAccount'}>
-                    <div className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/statusAccount" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
+                    <div onClick={() => setOpen(false)} className={`flex gap-[10px] items-center p-3 rounded-md mt-[5px] transition-colors duration-300 ${pathname === "/setting/statusAccount" ? "bg-gray-300" : "hover:bg-gray-200"}`}>
                         <UserRound />
                         <p>Статус аккаунта</p>
                     </div>
                 </Link>
+
             </div>
 
-            {/* Content */}
-            <div className="w-full md:flex-1 lg:w-[1000px]">
+            {open && (
+                <div
+                    className="fixed inset-0 bg-white bg-opacity-30 z-40 md:hidden"
+                    onClick={() => setOpen(false)}
+                ></div>
+            )}
+
+            <div className="md:hidden fixed top-4 left-4 z-50">
+                <button onClick={() => setOpen(true)}>
+                    <Menu size={28} />
+                </button>
+            </div>
+
+            <div className="w-full md:flex-1 lg:w-[1000px] overflow-auto">
                 {children}
             </div>
         </div>
